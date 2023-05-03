8af36f92dc4a72bdb35f76ddd0875b91dfc641ab
solamsalu <solamsalu@gmail.com>
2023-05-03 07:25:08 -0700

    Task done

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# Accept one argument
+input = ARGV[0]
+
+# Define a regular expression pattern
+pattern = /hbt+n/i
+
+# Match the input against the pattern
+if input.match(pattern)
+  puts ARGV[0].scan(pattern).join
+end
---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# Accept one argument
+input = ARGV[0]
+
+# Define a regular expression pattern
+pattern = /^\d{10}$/i
+
+# Match the input against the pattern
+if input.match(pattern)
+  puts ARGV[0].scan(pattern).join
+end
