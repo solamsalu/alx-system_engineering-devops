fc4e3febf43588e8686423bbf2662e7090e33c62
solamsalu <solamsalu@gmail.com>
2023-07-24 12:15:30 -0700

    Task done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-""""0. Write  a script using  REST API, for a given employee ID, returns information about his/her TODO list progress."""
+"""0. Write  a script using  REST API, for a given employee ID, returns information about his/her TODO list progress."""
 
 import requests
-import sys
+from sys import argv
 
-# Set the API endpoint URLs
-USERS_API_URL = "https://jsonplaceholder.typicode.com/users/{}"
-TODOS_API_URL = "https://jsonplaceholder.typicode.com/users/{}/todos"
 
-# Get the employee ID from the command line argument
-employee_id = sys.argv[1]
+def get_user(id):
+    """get the user
+    Args:
+        id (integer: user id]
+    """
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + 'users', params={'id': id}).json()
+    name = users[0]['name']
+    url = 'https://jsonplaceholder.typicode.com/'
+    todos = requests.get(url + 'todos', params={'userId': id}).json()
+    return([name, todos])
 
-# Send a GET request to the API and retrieve the JSON response
-response = requests.get(USERS_API_URL.format(employee_id))
-employee = response.json()
 
-# Get the employee name from the JSON response
-employee_name = employee["name"]
+def show(data):
+    name = data[0]
+    todos = data[1]
+    n = 0
+    str_to_print = ''
+    for task in todos:
+        if task['completed'] is True:
+            n += 1
+            str_to_print += '\t ' + task['title'] + '\n'
+    print('Employee {} is done with tasks({}/{}):'.format(name, n, len(todos)))
+    print(str_to_print, end='')
 
-# Send a GET request to the API and retrieve the JSON response
-response = requests.get(TODOS_API_URL.format(employee_id))
-todos = response.json()
 
-# Count the number of completed and total tasks
-total_tasks = len(todos)
-completed_tasks = sum(todo["completed"] for todo in todos)
-
-# Print the progress report header
-print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
-
-# Print the titles of completed tasks
-for todo in todos:
-    if todo["completed"]:
-        print("\t {} {}".format("\t", todo["title"]))
+if __name__ == '__main__':
+    data = get_user(argv[1])
+    show(data)
