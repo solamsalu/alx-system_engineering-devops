c105aed06845fefaf16b79ca73afeb04bcf8a8f6
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-08-08 23:53:35 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""
+Module for task 1
+"""
 
-"""Module for task 1"""
 import requests
 
 
@@ -10,9 +12,9 @@ def top_ten(subreddit):
     
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                             .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            headers={"User-Agent": "Custom User Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if sub_info.status_code != 200:
         print('None')
     else:
         [print(child.get("data").get("title"))
